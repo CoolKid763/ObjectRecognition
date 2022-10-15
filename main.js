2ea7f3b3f1b89e76ebcda420208d180abef8ec72
CoolKid763 <73192943+CoolKid763@users.noreply.github.com>
2022-10-15 14:23:18 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img = "";
 status = "";
+objects = [];
 
 function preload(){
     img = loadImage('dog_cat.jpg');
@@ -23,12 +24,27 @@ function gotResult(error, results){
         console.log(error);
     }
     console.log(results);
+    objects = results;
 }
 
 
 
 function draw(){
     image(img, 0, 0, 640, 420);
+
+    if(status != ""){
+        for(i = 0; 1 < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
+
+        }
+    }
     fill("#FF0000");
     text("Dog", 45, 75);
     noFill();
@@ -36,7 +52,7 @@ function draw(){
     rect(30, 60, 450, 350);
 
     fill("#FF0000");
-    text("cat", 320, 120);
+    text("Cat", 320, 120);
     noFill();
     stroke("#FF0000");
     rect(300, 90, 270, 320);
